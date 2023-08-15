cf0b74231f41c106654f85c72c2e9a8463e90b90
gsuyemoto <gsuyemoto@gmail.com>
2023-08-14 22:59:43 -0700

    Added 'PLAY' method. Right now, session token is not captured from 'SETUP' response and provided in 'PLAY'. Instead the sesssion token is left empty. This still seems to work with my test camera, Topdome.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
-use std::io::{Error, Read, Write};
+use anyhow::{Error, Result};
+use std::io::{Read, Write};
 use std::net::TcpStream;
+
+// #[derive(Debug, Deserialize)]
+// struct SessionDescription {
+//     #[serde(rename = "v")]
+//     version: u8,
+
+//     #[serde(rename = "o")]
+//     owner: String,
+
+//     #[serde(rename = "s")]
+//     session_name: String,
+
+//     #[serde(rename = "c")]
+//     connection: String,
+
+//     #[serde(rename = "t")]
+//     time: String,
+
+//     #[serde(rename = "a")]
+//     session_attribute_01: String,
+
+//     #[serde(rename = "a")]
+//     session_attribute_02: String,
+
+//     #[serde(rename = "m")]
+//     media_description: String,
+
+//     #[serde(rename = "a")]
+//     media_attribute_01: String,
+
+//     #[serde(rename = "a")]
+//     media_attribute_02: String,
+// }
 
 #[derive(Debug)]
 struct Session {
@@ -8,6 +42,7 @@ struct Session {
     stream: TcpStream,
     transport: String,
     track: String,
+    name: String,
 }
 
 impl Session {
@@ -17,13 +52,14 @@ impl Session {
             stream,
             transport: String::new(),
             track: String::new(),
+            name: String::new(),
             cseq: 1,
         }
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<()> {
     let rtsp_addr = "192.168.86.138:554";
 
     let mut session = Session::new(rtsp_addr.to_string(), TcpStream::connect(rtsp_addr)?);
@@ -32,7 +68,13 @@ async fn main() -> Result<(), Error> {
     println!("OPTIONS: \n{response}");
 
     let response = send_basic_rtsp_request(&mut session, "DESCRIBE").await?;
-    println!("DESCRIBE: \n{response}");
+
+    // Response from a DESCRIBE method will also have an SDP data
+    // SDP data begins after \r\n\r\n
+    let (headers, sdp) = response.split_once("\r\n\r\n").unwrap();
+
+    println!("DESCRIBE headers: {headers}");
+    println!("DESCRIBE session: {sdp}");
 
     session.transport = "Transport: RTP/AVP;unicast;client_port=4588-4589\r\n".to_string();
     session.track = "trackID=0".to_string();
@@ -40,13 +82,18 @@ async fn main() -> Result<(), Error> {
     let response = send_basic_rtsp_request(&mut session, "SETUP").await?;
     println!("SETUP: \n{response}");
 
+    session.name = "Session: ".to_string();
+
+    let response = send_basic_rtsp_request(&mut session, "PLAY").await?;
+    println!("PLAY: \n{response}");
+
     Ok(())
 }
 
 async fn send_basic_rtsp_request(sess: &mut Session, method: &str) -> Result<String, Error> {
     let request = format!(
-        "{} {}{} RTSP/1.0\r\nCSeq: {}\r\n{}\r\n",
-        method, sess.server_addr, sess.track, sess.cseq, sess.transport
+        "{} {}{} RTSP/1.0\r\nCSeq: {}\r\n{}\r\n{}\r\n",
+        method, sess.server_addr, sess.track, sess.cseq, sess.transport, sess.name,
     );
 
     let mut buffer = [0; 1024];
