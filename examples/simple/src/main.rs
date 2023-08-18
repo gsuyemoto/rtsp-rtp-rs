76de54fc20662ece6152e8861b4950bb37761267
gsuyemoto <gsuyemoto@gmail.com>
2023-08-18 01:36:29 -0700

    Unable to send NAL unit packets to OpenH264 decoder in correct way. Getting error on decode for every packet.

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use ctrlc;
-use openh264::{decoder::Decoder, nal_units};
+use openh264::{decoder::Decoder, decoder::DecoderConfig};
 use rtsp_client::{Methods, Session};
 use std::io::Cursor;
 use tokio::net::UdpSocket;
@@ -33,6 +33,11 @@ async fn main() -> Result<()> {
         // a different port
         udp_stream.connect("192.168.86.138:6600").await?;
 
+        // Setup OpenH264 decoder
+        let decoder_config = DecoderConfig::new();
+        decoder_config.debug(true);
+        let mut decoder = Decoder::with_config(decoder_config)?;
+
         // Set buffer to large enough to handle RTP packets
         // in my Wireshark analysis for this camera they
         // tended be a bit more than 1024
@@ -43,10 +48,13 @@ async fn main() -> Result<()> {
         // RTP packet. Some NAL units may not
         // contain enough info on their own and
         // may need more units, hence the buffer
-        let mut payload: Vec<u8> = Vec::new();
+        // let mut payload: Vec<u8> = Vec::new();
 
         // Capture X num fragments and then exit
         let mut sequence_started = false;
+
+        let mut num_packets = 0;
+        // let mut payload_buf: Vec<u8> = Vec::new();
 
         // Packet sequence for RTP using H264 and
         // packetization-mode=1 (non-interleaved mode)
@@ -71,57 +79,47 @@ async fn main() -> Result<()> {
         // Packet 8+ - More Coded Slices (NAL Type 1)-------|
         loop {
             let len = udp_stream.recv(&mut buf_rtp).await?;
+            // Byte 12 is NAL unit header
             let header_nal = &buf_rtp[12];
 
             println!("{} bytes received", len);
             println!("-----------\n{:08b}", header_nal);
 
             // Check if this is an SPS packet
-            // First byte should be -> 01100111
-            if *header_nal == 103u8 {
+            // First byte NAL type should be -> 00111
+            if *header_nal & 0b00000111 == 7u8 {
                 if sequence_started {
-                    // This is the end of the previous sequence
-                    // Attempt to decode with H264
-                    let mut decoder = Decoder::new()?;
-                    match decoder.decode(payload.as_slice()) {
-                        Ok(maybe_yuv) => match maybe_yuv {
-                            Some(yuv) => println!("Decoded YUV!"),
-                            None => println!("Unable to decode to YUV"),
-                        },
-                        Err(e) => eprintln!("Decoding error: {e}"),
-                    }
-
                     break;
                 } else {
+                    println!("Sequence started! --------------------------------------");
                     sequence_started = true;
                 }
             }
 
-            // If packetization-mode=1 found in SDP of Describe
-            // this is non-interleaved mode
-            // AND we find a packet of NAL type 28
-            // then server is sending FU-A type fragments
-            // AND each FU fragment has 2 byte headers
-            // So, NAL header 01111100 denotes FU-A fragment
-            if *header_nal == 124u8 {
-                // Get the 2nd byte for more header info
+            // Packetization-mode=1 found in SDP of Describe
+            // non-interleaved mode
+            // Packet of NAL type 28 denotes FU-A
+            // NAL type is last 5 bits -> 11100
+            // FU headers are 2 bytes
+            if *header_nal & 0b00011100 == 28u8 {
+                // Byte 13 is FU header
                 let header_fu = &buf_rtp[13];
-                println!("FU Header -----------\n{:08b}", header_fu);
+                println!("{:08b} -- {:08b}", header_nal, header_fu);
 
                 // Add FU payload to buffer which is
                 // RTP packet minus RTP header minus FU header
-                // = packet - 12u8 - 2u8
-                // = packet - 14
-                payload.extend_from_slice(&buf_rtp[14..len]);
-                println!("FRAGMENT packet received. Buffer length: {}", payload.len());
+                // payload = packet - 14 bytes
+                // payload.extend_from_slice(&buf_rtp[14..len]);
+                // payload.extend_from_slice(&buf_rtp[12..len]);
+                // println!("FRAGMENT packet received. Buffer length: {}", payload.len());
 
                 // Look for an IDR fragment
                 // which is detemined by NAL type in last 5 bits
                 // IDR is NAL type 5 which is 101 for last 5 bits
 
-                // FU header = 10000101 -- fragment start
-                // FU header = 00000101 -- fragment middle
-                // FU header = 01000101 -- fragment end
+                // FU header = 10000101 -- IDR fragment start
+                // FU header = 00000101 -- IDR fragment middle
+                // FU header = 01000101 -- IDR fragment end
                 if *header_fu == 133u8 || *header_fu == 69u8 || (*header_fu == 5u8) {
                     // End of fragment, try to decode
                     if *header_fu == 69u8 {}
@@ -131,8 +129,29 @@ async fn main() -> Result<()> {
                 // header and this header can be longer
                 // depending on CC flag bit
                 // header.len() == 12 + (CC * 4)
-                payload.extend_from_slice(&buf_rtp[12..len]);
-                println!("Non fragment packet. Buffer length: {}", payload.len());
+                // payload = packet - 12 bytes
+                // payload.extend_from_slice(&buf_rtp[12..len]);
+                // println!("Non fragment packet. Buffer length: {}", payload.len());
+            }
+
+            num_packets += 1;
+            // payload_buf.extend_from_slice(&buf_rtp[12..len]);
+            // println!("Payload buf size: {}", payload_buf.len());
+
+            // if sequence_started && num_packets == 3 {
+            // Attempt to decode with H264
+            match decoder.decode(&buf_rtp[12..len]) {
+                Ok(maybe_yuv) => match maybe_yuv {
+                    Some(yuv) => println!("Decoded YUV!"),
+                    None => println!("Unable to decode to YUV"),
+                },
+                Err(e) => eprintln!("Decoding error: {e}"),
+            }
+
+            // }
+
+            if num_packets == 40 {
+                break;
             }
         }
     }
